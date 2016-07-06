4143baaaf35fe483d6c4f54d9b707038e319867f
oprogramador <uomodislesia@gmail.com>
2016-07-06 23:15:48 +0200

    rest api

--- backend/db/taskRepository.js
+++ backend/db/taskRepository.js
@@ -2,20 +2,22 @@ var taskRepository = {};
 var connector = require('./connector.js');
 var mongo = require('mongodb');
 
-taskRepository.add = function(task) {
+taskRepository.add = function(task, callback) {
   task['isDone'] = false;
   connector.connect(function(err, db) {
     var tasks = db.collection('tasks');
     tasks.insert(task);
+    callback(task);
   });
 }
 
-taskRepository.changeStatus = function(taskId) {
+taskRepository.changeStatus = function(taskId, callback) {
   connector.connect(function(err, db) {
     var tasks = db.collection('tasks');
     db.collection('tasks').find({_id: new mongo.ObjectId(taskId)}).snapshot().forEach(function(task) {
       task.isDone = !task.isDone;
       tasks.save(task);
+      callback(task);
     });
   });
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var taskRepository = require('../db/taskRepository.js');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({success: true});
+router.post('/task', function(req, res, next) {
+  var msg = req.body.msg;
+  var date = new Date(req.body.date);
+  taskRepository.add({msg: msg, date: date}, function(task) {
+    res.json(task);
+  });
+});
+
+router.put('/task/changeStatus/:id', function(req, res, next) {
+  var id = req.params['id'];
+  taskRepository.changeStatus(id, function(task) {
+    res.json(task);
+  });
+});
+
+router.get('/task/all', function(req, res, next) {
+  taskRepository.findAll(function(result) {
+    res.json(result);
+  });
 });
 
 module.exports = router;
